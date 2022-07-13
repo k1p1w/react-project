21ce4aadefd2b9ad017278eb4298669266f7a53f
k1p1w <k1p1wzbs@gmail.com>
2022-07-14 02:27:56 +0300

    test

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,16 @@
+import { Col, Container, Row } from "react-bootstrap"
+
 export const Banner = () => {
 	return (
-		<section
+		<section className="Banner" id="home">
+			<Container>
+				<Row className="align-items-center">
+					<Col xs={12} md={6} xl={7}>
+						<span className="tagline">Welcome to my Portfolio</span>
+						<h1>{`Hi I'm k1p1w`}<span className="wrap">web developer</span></h1>
+					</Col>
+				</Row>
+			</Container>
+		</section>
 	)
 }
