c25578b29b63cb1f971d45b97c4dcd04d492caaf
k1p1w <k1p1wzbs@gmail.com>
2022-07-15 23:35:16 +0300

    test k1p1w portfolio

---
+++ src/components/Skills.js
@@ -0,0 +1,86 @@
+import { Col, Container, Row } from 'react-bootstrap';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import meter1 from '../assets/img/meter1.png';
+import meter2 from '../assets/img/meter2.png';
+import meter3 from '../assets/img/meter3.png';
+import meter4 from '../assets/img/meter4.png';
+import colorSharp from '../assets/img/color-sharp.png';
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <section className="skill" id="skills">
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-box">
+              <h2>Skills</h2>
+              <p>
+                On the other hand, we denounce with righteous indignation and
+                dislike men who are so beguiled and demoralized by the charms of
+                pleasure of the moment, so blinded by desire, that they cannot
+                foresee the pain and trouble that are bound to ensue; and equal
+                blame belongs to those who fail in their duty through weakness
+                of will, which is the same as saying through shrinking from toil
+                and pain. These cases are perfectly simple and easy to
+                distinguish. In a free hour, when our power of choice is
+                untrammelled and when nothing prevents our being able to do what
+                we like best, every pleasure is to be welcomed and every pain
+                avoided. But in certain circumstances and owing to the claims of
+                duty or the obligations of business it will frequently occur
+                that pleasures have to be repudiated and annoyances accepted.
+                The wise man therefore always holds in these matters to this
+                principle of selection: he rejects pleasures to secure other
+                greater pleasures, or else he endures pains to avoid worse
+                pains.
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="skill-slider"
+              >
+                <div className="item">
+                  <img src={meter1} alt="Image" />
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="Image" />
+                  <h5>HTML/CSS/JavaScript</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="Image" />
+                  <h5>Git/Github/Node.js</h5>
+                </div>
+                <div className="item">
+                  <img src={meter4} alt="Image" />
+                  <h5>Web Design</h5>
+                </div>
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-left" src={colorSharp} />
+    </section>
+  );
+};
