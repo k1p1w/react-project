91f9e1bb66fccb174aaea0cbc76790dbfe46fb06
k1p1w <k1p1wzbs@gmail.com>
2022-07-17 23:30:09 +0300

    test k1p1w portfolio

---
+++ src/components/Projects.js
@@ -0,0 +1,76 @@
+import { Col, Container, Row, Tab, Nav } from 'react-bootstrap';
+import { ProjectCard } from './ProjectCard';
+// import colorSharp2 from '../assets/img/color-sharp2.png';
+import projImg1 from '../assets/img/project-img1.png';
+import projImg2 from '../assets/img/project-img2.png';
+import projImg3 from '../assets/img/project-img3.png';
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg1,
+    },
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg2,
+    },
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>
+              On the other hand, we denounce with righteous indignation and
+              dislike men who are so beguiled and demoralized by the charms of
+              pleasure of the moment, so blinded by desire, that they cannot
+              foresee the pain and trouble that are bound to ensue; and equal
+              blame belongs to those who fail in their duty through weakness of
+              will, which is the same as saying through shrinking from toil and
+              pain.
+            </p>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">to be continued</Tab.Pane>
+                <Tab.Pane eventKey="third">to be continued</Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      {/* <img className="backgraund-image-right" src={colorSharp2}></img> */}
+    </section>
+  );
+};
